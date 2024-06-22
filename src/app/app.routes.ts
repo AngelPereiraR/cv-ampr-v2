4c33489a4801ebc06931ff1020662765d337b0e3
Ángel Pereira <ampr2003@gmail.com>
2024-06-22 14:52:00 +0200

    Inicio proyecto CV

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './cv/pages/index/index.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", component: IndexComponent}
+];
