e719e62968a2df3c14033098ce8d2b92d2e51e87
Ángel Pereira <ampr2003@gmail.com>
2024-06-27 16:37:29 +0200

    Animaciones de Hover

--- src/app/cv/pages/index/index.component.ts
+++ src/app/cv/pages/index/index.component.ts
@@ -3,7 +3,7 @@ import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
 import { HomeComponent } from '../../components/home_component/home.component';
 import { ServicesComponent } from '../../components/services_component/services.component';
 import { AboutComponent } from '../../components/about_component/about.component';
-import { ResumeComponent } from '../../components/resume/resume.component';
+import { ResumeComponent } from '../../components/resume_component/resume.component';
 import { ProjectsComponent } from '../../components/projects_component/projects.component';
 
 @Component({
