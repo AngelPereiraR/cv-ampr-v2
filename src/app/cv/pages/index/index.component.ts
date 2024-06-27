121b1dbadea159e7a8edf62164002d7816495a32
Ángel Pereira <ampr2003@gmail.com>
2024-06-27 16:13:38 +0200

    Componente Contacto eliminado

--- src/app/cv/pages/index/index.component.ts
+++ src/app/cv/pages/index/index.component.ts
@@ -1,18 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
-import { HomeComponent } from "../../components/home_component/home.component";
-import { ServicesComponent } from "../../components/services_component/services.component";
-import { AboutComponent } from "../../components/about_component/about.component";
-import { ResumeComponent } from "../../components/resume/resume.component";
-import { ProjectsComponent } from "../../components/projects_component/projects.component";
-import { ContactComponent } from "../../components/contact_component/contact.component";
+import { HomeComponent } from '../../components/home_component/home.component';
+import { ServicesComponent } from '../../components/services_component/services.component';
+import { AboutComponent } from '../../components/about_component/about.component';
+import { ResumeComponent } from '../../components/resume/resume.component';
+import { ProjectsComponent } from '../../components/projects_component/projects.component';
 
 @Component({
-    selector: 'app-index',
-    standalone: true,
-    templateUrl: './index.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [CommonModule, HomeComponent, ServicesComponent, AboutComponent, ResumeComponent, ProjectsComponent, ContactComponent]
+  selector: 'app-index',
+  standalone: true,
+  templateUrl: './index.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    CommonModule,
+    HomeComponent,
+    ServicesComponent,
+    AboutComponent,
+    ResumeComponent,
+    ProjectsComponent,
+  ],
 })
 export class IndexComponent implements OnInit {
   ngOnInit(): void {}
